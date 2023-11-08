8a1a3e56473b48eedba7e26367f44652f6178175
CreeperG16 <pappt1216@gmail.com>
2023-11-08 16:05:39 +0100

    0.2.0

--- main.lua
+++ main.lua
@@ -1,4 +1,83 @@
---[[
-  Main code goes here.
-  local name = require "path/to/file.lua" To import code from other files
-]]
+renderEverywhere = true
+
+local settings = {
+  client.settings.addCategory("View settings"),
+  ---@diagnostic disable-next-line: undefined-global
+  toggleView = client.settings.addNamelessKeybind("Toggle view", KeyCodes.LeftAlt),
+  colouredShulkers = client.settings.addNamelessBool("Coloured shulkers", true),
+  defaultView = client.settings.addNamelessEnum("Default view", 1, {{1, "Item count"}, {2, "Full view"}}),
+  countView = client.settings.addNamelessEnum("Count format", 1, {{1, "Total items"}, {2, "Total stacks (rounded up)"}, {3, "Total stacks (rounded down)"}}),
+  client.settings.stopCategory()
+}
+
+MX, MY = gui.mousex, gui.mousey
+local toggleView = false
+local shulkerBoxes = {}
+
+function mouseIn(x, y, w, h)
+  return (x <= MX() and MX() < x + w) and (y <= MY() and MY() < y + h)
+end
+
+function screenIs(...)
+  for _, s in ipairs({ ... }) do if gui.screen() == s then return true end end
+  return false
+end
+
+local shulkerItem = require "./util/shulker-item.lua"
+local renderShulker = require "./render.lua"
+
+event.listen("MouseInput", function(button, down)
+  if not (button == 1 and down) then return end
+
+  if
+    mouseIn(inventory.craftingBook.position()) and
+    screenIs("inventory_screen", "crafting_screen")
+  then
+    -- Janky but kinda works as long as you don't mess around with it too much
+    inventory.craftingBook.toggle()
+  end
+end)
+
+event.listen("KeyboardInput", function(key, down)
+  if key == settings.toggleView.value then toggleView = down end
+end)
+
+function render()
+  shulkerBoxes = shulkerItem()
+
+  gfx.text(10, 10, gui.screen())
+
+  -- TODO: implement for other screens
+  if not screenIs(
+    "inventory_screen",
+    "crafting_screen",
+    "small_chest_screen",
+    "ender_chest_screen",
+    "barrel_screen",
+    "large_chest_screen",
+    "furnace_screen",
+    "blast_furnace_screen",
+    "smoker_screen",
+    "dropper_screen",
+    "dispenser_screen",
+    "hopper_screen",
+    "anvil_screen",
+    "loom_screen",
+    "enchanting_screen",
+    "cartography_screen",
+    "beacon_screen",
+    "trade_screen",
+    "horse_screen", -- Breaks on donkeys/llamas with less than max storage capacity
+    "brewing_stand_screen",
+    "smithing_table_screen",
+    "grindstone_screen",
+    "stonecutter_screen"
+  ) then return end
+
+  for slot, shulker in pairs(shulkerBoxes) do
+    if mouseIn(inventory.coords(slot)) then
+      renderShulker(shulker, toggleView == (settings.defaultView.value == 2))
+      break
+    end
+  end
+end
---
+++ render.lua
@@ -0,0 +1,145 @@
+---@diagnostic disable: undefined-global
+
+-- Thanks onix for the tinting stuff
+local colourMult = { r = 1, g = 1, b = 1, a = 1 }
+local function setColour(r, g, b, a) gfx.color(r * colourMult.r, g * colourMult.g, b * colourMult.b, (a or 255) * colourMult.a) end
+local function setTint(r, g, b, a) colourMult = { r = (r or 255) / 255, g = (g or 255) / 255, b = (b or 255) / 255, a = (a or 255) / 255 } end
+
+local COLOURS = {
+  white = "e4e9e9",
+  light_gray = "8b8282",
+  gray = "3e4246",
+  black = "1f1f23",
+  brown = "724728",
+  red = "9a2422",
+  orange = "f4730f",
+  yellow = "fcc724",
+  lime = "71bc18",
+  green = "546d1c",
+  cyan = "168691",
+  light_blue = "3ab3da",
+  blue = "33359b",
+  purple = "7426a9",
+  magenta = "b93eae",
+  pink = "f38aa9",
+
+  undyed = "ffffff"
+}
+
+local function renderBackground(x, y, w, h)
+  setColour(0xc6, 0xc6, 0xc6)
+  gfx.rect(x, y, w, h)
+
+  setColour(0xff, 0xff, 0xff)
+  gfx.rect(x, MY() - h - 12, w, 2)
+  gfx.rect(x, MY() - h - 12, 2, h)
+
+  setColour(0x55, 0x55, 0x55, 128)
+  gfx.rect(x + w - 2, y, 2, h)
+  gfx.rect(x, y + h - 2, w, 2)
+
+  setColour(0, 0, 0)
+  gfx.drawRect(x - 1, MY() - h - 13, w + 2, h + 2, 1)
+end
+
+local function itemCountText(count, slotX, slotY)
+  if count == 1 then return end
+  local countText = tostring(count)
+
+  if settings.countView.value == 2 then
+    countText = tostring((count + 63) // 64) .. "s"
+  elseif settings.countView.value == 3 then
+    local stacks = count // 64
+    countText = stacks == 0 and tostring(count) or (tostring(stacks) .. "s")
+  end
+
+  -- Doesn't return true when it should as of now :|
+  local textScale = gui.font().isMinecrafttia and 1 or 1.2
+
+  local tW, tH = gui.font().width(countText, textScale), gui.font().height * textScale
+  gfx.color(255, 255, 255)
+  gfx.text(slotX + 17 - tW, slotY + 17 - tH, countText, textScale)
+end
+
+local function drawSlot(slotX, slotY)
+  setColour(0x8b, 0x8b, 0x8b)
+  gfx.rect(slotX, slotY, 16, 16)
+
+  setColour(0x37, 0x37, 0x37)
+  gfx.rect(slotX - 1, slotY - 1, 1, 18)
+  gfx.rect(slotX - 1, slotY - 1, 18, 1)
+
+  setColour(0xff, 0xff, 0xff)
+  gfx.rect(slotX - 1, slotY + 16, 18, 1)
+  gfx.rect(slotX + 16, slotY - 1, 1, 18)
+end
+
+local function renderItemCounts(shulker, bgW, bgH, renderLeft)
+  local i = 0
+
+  for _, itemCount in ipairs(shulker.itemCounts) do
+    local slotX = (MX() + 16) + 18 * (i % 9)
+    local slotY = (MY() - bgH - 7) + 18 * (i // 9)
+
+    if renderLeft then slotX = slotX - bgW - 22 end
+
+    drawSlot(slotX, slotY)
+
+    local item = getItem(itemCount[1])
+    item.count = itemCount[2]
+
+    ---@diagnostic disable-next-line: param-type-mismatch
+    gfx.item(slotX, slotY, item)
+
+    itemCountText(itemCount[2], slotX, slotY)
+
+    i = i + 1
+  end
+end
+
+local function renderItemSlots(shulker, bgW, bgH, renderLeft)
+  for i = 0, 26 do
+    local slotX = (MX() + 16) + 18 * (i % 9)
+    local slotY = (MY() - bgH - 7) + 18 * (i // 9)
+
+    if renderLeft then slotX = slotX - bgW - 22 end
+
+    drawSlot(slotX, slotY)
+
+    if not shulker.items[i] then goto cont end
+
+    local item = itemFromNbt(shulker.items[i])
+
+    ---@diagnostic disable-next-line: param-type-mismatch
+    gfx.item(slotX, slotY, item, 1, true)
+
+    ::cont::
+  end
+end
+
+return function(shulker, counts)
+  local bgW = (counts and (#shulker.itemCounts < 9 and #shulker.itemCounts or 9) or 9) * 18 + 8
+  local bgH = (counts and #shulker.itemCounts // 9 + 1 or 3) * 18 + 8
+
+  local bgX, bgY = MX() + 11, MY() - bgH - 12
+
+  local renderLeft = bgX + bgW > gui.width()
+  if renderLeft then bgX = MX() - bgW - 11 end
+
+  if shulker.colour and settings.colouredShulkers.value then
+    local hex = tonumber(COLOURS[shulker.colour], 16)
+    local r, g, b = (hex & 0xff0000) >> 16, (hex & 0xff00) >> 8, hex & 0xff
+
+    setTint(r, g, b)
+  else
+    setTint(255, 255, 255)
+  end
+
+  renderBackground(bgX, bgY, bgW, bgH)
+
+  if counts then
+    renderItemCounts(shulker, bgW, bgH, renderLeft)
+  else
+    renderItemSlots(shulker, bgW, bgH, renderLeft)
+  end
+end
